d74be400629dcc0a9eab2f100e62b326371bed85
Naman13112004 <njudaipur04@gmail.com>
2025-09-29 17:14:28 +0530

    8: creating transaction table and filling DB with dummy transaction data

--- src/app/(main)/account/[id]/page.tsx
+++ src/app/(main)/account/[id]/page.tsx
@@ -1,6 +1,11 @@
+import { Suspense } from "react";
+import { BarLoader } from "react-spinners";
+
 import { headers } from "next/headers";
 import { notFound } from "next/navigation";
 import { Account, Transaction } from "@/generated/prisma";
+
+import TransactionTable from "@/components/transactions/transaction-table";
 
 type AccountPageProps = {
     params: {
@@ -32,28 +37,35 @@ const AccountPage = async ({ params }: AccountPageProps) => {
   const { account, transactions } : {account: AccountWithTransactions, transactions: Transaction[]} = await accountData.json();
 
   return (
-    <div className="space-y-8 px-5 flex gap-4 items-end justify-between">
-        <div>
-            <h1 className="text-5xl sm:text-6xl font-bold gradient gradient-title capitalize">
-                {account.name}
-            </h1>
-            <p className="text-muted-foreground">
-                {account.type.charAt(0) + account.type.slice(1).toLowerCase()} Account
-            </p>
-        </div>
-
-        <div className="text-right pb-7">
-            <div className="text-xl sm:text-2xl font-bold">
-                Rs. {Number(account.balance).toFixed(2)}
+    <div className="space-y-8 px-5">
+        <div className="flex gap-4 items-end justify-between">
+            <div>
+                <h1 className="text-5xl sm:text-6xl font-bold gradient gradient-title capitalize">
+                    {account.name}
+                </h1>
+                <p className="text-muted-foreground">
+                    {account.type.charAt(0) + account.type.slice(1).toLowerCase()} Account
+                </p>
             </div>
-            <p className="text-sm text-muted-foreground">
-                {account._count.transactions} Transactions
-            </p>
+
+            <div className="text-right pb-7">
+                <div className="text-xl sm:text-2xl font-bold">
+                    Rs. {Number(account.balance).toFixed(2)}
+                </div>
+                <p className="text-sm text-muted-foreground">
+                    {account._count.transactions} Transactions
+                </p>
+            </div>
         </div>
 
         {/* Chart Section */}
 
         {/* Transaction Table */}
+        <Suspense 
+            fallback={<BarLoader className="mt-4" width={"100%"} color="#9333ea" />}
+        >
+            <TransactionTable transactions={transactions} />
+        </Suspense>
     </div>
   );
 };
