f28b52333d3feb1ac2f0d8b69e55fb986ff146db
Naman13112004 <njudaipur04@gmail.com>
2025-11-04 15:40:03 +0530

    9: filter and delete transactions

--- src/app/api/transactions/route.ts
+++ src/app/api/transactions/route.ts
@@ -54,3 +54,68 @@ export async function GET(req: Request) {
         return NextResponse.json({ error: "Internal server error" }, { status: 500 });
     }
 }
+
+export async function DELETE(req: Request) {
+    try {
+        const { userId } = await auth();
+
+        if(!userId) {
+            return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+        }
+
+        const user = await db.user.findUnique({
+            where: { clerkUserId: userId },
+        });
+
+        if(!user) {
+            return NextResponse.json({ error: "User not found" }, { status: 404 });
+        }
+
+        const body = await req.json();
+        const transactionIds: string[] = body.transactionIds;
+
+        const transactions = await db.transaction.findMany({
+            where: {
+                id: { in: transactionIds },
+                userId: user.id,
+            },
+        });
+
+        const accountBalanceChanges = transactions.reduce<Record<string, number>>((acc, transaction) => {
+            const change = 
+                transaction.type === "EXPENSE"
+                    ? transaction.amount
+                    : -transaction.amount;
+
+            acc[transaction.accountId] = (acc[transaction.accountId] || 0) + Number(change);
+            return acc;
+        }, {});
+
+        // Delete transactions and update account balances in a transaction
+        await db.$transaction(async (tx) => {
+            await tx.transaction.deleteMany({
+                where: {
+                    id: { in: transactionIds },
+                    userId: user.id,
+                },
+            });
+
+            for(const [accountId,balanceChange] of Object.entries(accountBalanceChanges)) {
+                await tx.account.update({
+                    where: { id: accountId },
+                    data: {
+                        balance: {
+                            increment: balanceChange,
+                        },
+                    },
+                });
+            }
+
+        });
+        
+        return NextResponse.json({ message: "Accounts deleted successfully" }, { status: 201 });
+
+    } catch (error) {
+        return NextResponse.json({ error: "Internal server error" }, { status: 500 });
+    }
+}
