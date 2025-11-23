81891fdb3169b2c2037049f17a52cc5b1bab2487
Naman13112004 <njudaipur04@gmail.com>
2025-11-23 19:23:57 +0530

    11: monthly budget feature added

---
+++ src/app/api/budget/route.ts
@@ -0,0 +1,101 @@
+import { db } from "@/lib/prisma";
+import { auth } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+export async function GET(req: Request) {
+    try {
+        const { userId } = await auth();
+        if(!userId) {
+            return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+        }
+
+        const user = await db.user.findUnique({
+            where: { clerkUserId: userId },
+        });
+
+        if(!user) {
+            return NextResponse.json({ error: "User not found" }, { status: 404 });
+        }
+
+        const budget = await db.budget.findFirst({
+            where: { userId: user.id },
+        });
+
+        const currentDate = new Date();
+        const startOfMonth = new Date(
+            currentDate.getFullYear(), 
+            currentDate.getMonth(), 
+            1
+        );
+        const endOfMonth = new Date(
+            currentDate.getFullYear(), 
+            currentDate.getMonth() + 1, 
+            0
+        );
+
+        const { searchParams } = new URL(req.url);
+        const accountId = searchParams.get("accountId");
+
+        if (!accountId) {
+            return NextResponse.json({ error: "Account ID required" }, { status: 400 });
+        }
+
+        const totalExpenses = await db.transaction.aggregate({
+            where: {
+                userId: user.id,
+                createdAt: {
+                    gte: new Date(2025, 8, 1),
+                    lte: new Date(2025, 8, 31),
+                },
+                type: "EXPENSE",
+                accountId: accountId,
+            },
+            _sum: {
+                amount: true,
+            },
+        });
+
+        return NextResponse.json(
+            { 
+                budget: budget ? { ...budget, amount: budget.amount.toNumber() } : null,
+                currentExpenses: totalExpenses._sum.amount ? totalExpenses._sum.amount.toNumber() : 0,
+            }, 
+            { status: 201 }
+        );        
+
+    } catch (error) {
+        return NextResponse.json({ error: "Internal server error" }, { status: 500 });        
+    }
+}
+
+export async function PUT(req: Request) {
+    try {
+        const { userId } = await auth();
+        if(!userId) {
+            return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+        }
+
+        const user = await db.user.findUnique({
+            where: { clerkUserId: userId },
+        });
+
+        if(!user) {
+            return NextResponse.json({ error: "User not found" }, { status: 404 });
+        }
+
+        const { amount } = await req.json();
+
+        const budget = await db.budget.upsert({
+            where: { userId: user.id },
+            update: { amount },
+            create: { amount, userId: user.id },
+        });
+
+        return NextResponse.json(
+            { budget: { ...budget, amount: budget.amount.toNumber(), success: true } }, 
+            { status: 201 }
+        );
+    } catch (error) {
+        return NextResponse.json({ error: "Internal server error" }, { status: 500 });
+    }
+}
