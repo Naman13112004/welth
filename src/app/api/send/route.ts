bb39663c4b48c1d44ea1ee501f6851a83a94b486
Naman13112004 <njudaipur04@gmail.com>
2025-11-24 11:30:57 +0530

    12: added cronjob for budget alert

---
+++ src/app/api/send/route.ts
@@ -0,0 +1,35 @@
+import { Resend } from 'resend';
+import { NextResponse } from 'next/server';
+import EmailTemplate from '../../../../emails/template';
+
+const resend = new Resend(process.env.RESEND_API_KEY || "");
+
+export async function POST(req: Request) {
+    const { to, subject, templateData } = await req.json();
+
+    try {
+        const { data, error } = await resend.emails.send({
+            from: 'Finance App <onboarding@resend.dev>',
+            to,
+            subject,
+            react: EmailTemplate(templateData),
+        });
+
+        if (error) {
+            return NextResponse.json({ error }, { status: 500 });
+        }
+
+        return NextResponse.json({
+            success: true,
+            data,
+        });
+    } catch (error) {
+        return NextResponse.json(
+            { 
+                success: false,
+                error,
+            }, 
+            { status: 500 }
+        );
+    }
+}
